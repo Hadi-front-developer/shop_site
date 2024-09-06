db2595b70d685ee9794a2d7301950c90ac0541f4
Hadi <jafarihadi661@gamil.com>
2024-09-06 12:10:46 -0700

    fix all of the site

--- src/script.js
+++ src/script.js
@@ -51,6 +51,7 @@ var swiper = new Swiper(".ndSwiper", {
      slidesPerView: 2,
      spaceBetween: 30,
      loop: true,
+     // centeredSlides: true,
      autoplay: {
           delay: 3000,
           disableOnInteraction: false,
@@ -64,3 +65,24 @@ var swiper = new Swiper(".ndSwiper", {
           },
      },
 });
+
+// clock if wonder
+
+setInterval(() => {
+
+     let date = new Date();
+     let h = date.getHours();
+     let m = date.getMinutes();
+     let s = date.getSeconds();s
+     h = h < 10 ? '0' + h : h;
+     m = m < 10 ? '0' + m : m;
+     s = s < 10 ? '0' + s : s;
+
+     document.getElementById('hor').innerHTML = h;
+     document.getElementById('min').innerHTML = m;
+     document.getElementById('sec').innerHTML = s;
+
+}, 1000);
+
+
+
